09539d86e30fb975fae452bb0b569bc8709b5584
Parul Gupta <parulguptasmail@gmail.com>
2018-03-13 18:57:24 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +83,8 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = {}
+ 
 end
 
 def changing_alan
